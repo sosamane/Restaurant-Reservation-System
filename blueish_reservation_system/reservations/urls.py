c3300764c2fb51505fa0b1cb3886990bf75c205b
SuhMunir <159935678+SuhMunir@users.noreply.github.com>
2025-04-07 18:11:40 +0100

    Delete blueish_reservation_system/reservations directory

--- blueish_reservation_system/reservations/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path #imports path utility
-from . import views #imports views
-
-urlpatterns = [
-    path('make-booking/', views.make_reservation, name='make_reservation'), #reservations/make-booking - make reservation
-    path('booking-confirmation/', views.reservation_complete, name='reservation_complete'), #reservations/booking-confirmation - booking completed
-]
